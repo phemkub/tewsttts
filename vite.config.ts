e056b32c53bfd6bbdc2296fd965d0c0cb60403a5
phemkub <phemtaochinkub@gmail.com>
2026-04-02 21:16:13 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
-  // Important for GitHub Pages project site: https://<user>.github.io/KATOON/
-  base: '/KATOON/',
+  base: '/tewsttfs/',
   plugins: [react()],
 })
