8a782413fdae8b08396a27e70649e1de22f89142
phemkub <phemtaochinkub@gmail.com>
2026-04-02 23:03:18 +0700

    Update content.ts

--- src/config/content.ts
+++ src/config/content.ts
@@ -12,7 +12,7 @@ export const content = {
   ],
   
   letterClosing: 'Happy Birthday นะ คนเก่งของเค้า',
-  finalSongTitle: 'กำลังเล่น: Superpowers - Daniel Caesar ',
+
   finalSongUrl: 'superpowers.mp3',
   galleryImageUrls: [
     'https://i.ibb.co/r2WdmCmN/1775098202860.jpg',
