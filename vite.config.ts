89e0b8b92263a6f4a17cd5056989a7909ef5b37a
phemkub <phemtaochinkub@gmail.com>
2026-04-02 21:13:07 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Important for GitHub Pages project site: https://<user>.github.io/KATOON/
+  base: '/KATOON/',
   plugins: [react()],
 })
